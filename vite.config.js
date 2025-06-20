bb9700b1f0ccde4f2f55c1db3dbe8e8f821bc2b1
Eric Amigo <ericamigo.dev@gmail.com>
2025-06-21 00:05:09 +0800

    pint and vite
    Signed-off-by: Eric Amigo <ericamigo.dev@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-import vue from '@vitejs/plugin-vue';
-import laravel from 'laravel-vite-plugin';
-import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
         vue({
             template: {
                 transformAssetUrls: {
@@ -20,10 +18,12 @@ export default defineConfig({
                 },
             },
         }),
+        tailwindcss(),
     ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './resources/js'),
         },
     },
-});
+})
+
